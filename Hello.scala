931b4170bd29bc6d7210a49b2b71275aa6113553
larce <luis.arce22@gmail.com>
2016-12-19 09:49:14 -0400

    Ping Pong example

--- Hello.scala
+++ Hello.scala
@@ -1,20 +1,61 @@
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
+import akka.actor._
+import akka.actor.Actor._
 
-class HelloActor extends Actor {
+class HelloActor(myName: String) extends Actor {
   def receive = {
-    case "hello" => println("hello back at you")
-    case _       => println("huh?")
+    case "hello" => println("hello from %s".format(myName))
+    case _ => println("'huh?', said %s".format(myName))
   }
 }
 
 object Main extends App {
-	  val system = ActorSystem("HelloSystem")
-	  // default Actor constructor
-	  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
-	  helloActor ! "hello"
-	  helloActor ! "buenos dias"
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
 }
 
-//https://www.toptal.com/scala/concurrency-and-fault-tolerance-made-easy-an-intro-to-akka
+case object Ping
+case object Pong
+
+class PongActor extends Actor {
+  def receive = {
+    case Ping ⇒ {
+      Thread.sleep(2000)
+      println(self.path + ": Received Ping!")
+      sender ! Pong
+    }
+    case _ ⇒ ()
+  }
+}
+
+class PingActor extends Actor {
+
+  context.actorSelection("../Pong*") ! Ping // starts things off
+
+  def receive = {
+    case Pong ⇒ {
+      Thread.sleep(2000)
+      println(self.path + ": Received Pong!")
+      sender ! Ping
+    }
+    case _ ⇒ ()
+  }
+}
+
+object PingPong extends App {
+  val system = ActorSystem()
+  system.actorOf(Props[PongActor], name = "Pong")
+  system.actorOf(Props[PingActor], name = "Ping")
+}
+
+object PingPongPong extends App {
+  val system = ActorSystem()
+  system.actorOf(Props[PongActor], name = "Pong1")
+  system.actorOf(Props[PongActor], name = "Pong2")
+  system.actorOf(Props[PingActor], name = "Ping")
+}
